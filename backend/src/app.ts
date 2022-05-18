ffd3f219ceaed07be103d58c9466cf1c72844828
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-18 18:21:24 -0300

    :heavy_plus_sign: build: Arquivos app.ts inicial e server.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,31 @@
+import * as express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+
+    this.startConfigs();
+  }
+
+  private startConfigs(): void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(accessControl);
+  }
+
+  public start(PORT: number): void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
+
+export const { app } = new App();
