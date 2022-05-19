d37308ea4a62d7d9e41975f0dfecdff40558cb54
Ismael Peccin <ismaelpeccin@gmail.com>
2022-05-19 13:55:04 -0300

    : heavy_plus_sign : build: Criando Migrations

---
+++ backend/src/database/migrations/20220519164410-tasks.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('tasks', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      task: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'created_at',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'updated_at',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      }
+    }); 
+  },
+  
+  down: async (queryInterface) => {
+   await queryInterface.dropTable('tasks');
+  }
+};
